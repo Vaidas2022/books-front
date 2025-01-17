cf090d40f69f45f6f07cb849ee2706b32c768aaf
Vaidas Cesonis <vaices1974@gmail.com>
2025-01-17 09:40:07 +0200

    Made changes to have global export of Books components

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+export { default as BookList } from './BookList';
+export { default as BookForm } from './BookForm';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
