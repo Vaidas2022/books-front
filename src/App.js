f6db262e43c894341171ddfdf374cd768cda23ed
Vaidas Cesonis <vaices1974@gmail.com>
2025-01-17 09:47:30 +0200

    Moved components to proper places and added functionality to App with Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BookList, BookForm } from './components/books';
+import { AuthorList, AuthorForm } from './components/authors';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <Router>
+<div>
+<nav>
+<ul>
+<li><Link to="/books">Knygos</Link></li>
+<li><Link to="/authors">Autoriai</Link></li>
+</ul>
+</nav>
+<Routes>
+<Route path="/books" element={<BookList />} />
+<Route path="/books/add" element={<BookForm />} />
+<Route path="/books/edit/:id" element={<BookForm isEdit={true} />} />
+<Route path="/authors" element={<AuthorList />} />
+<Route path="/authors/add" element={<AuthorForm />} />
+<Route path="/authors/edit/:id" element={<AuthorForm isEdit={true} />} />
+</Routes>
+</div>
+</Router>
   );
 }
 
