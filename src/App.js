aa5f87d7cc3583c7de7adb3748a4642f84ee2e4c
Vaidas Cesonis <vaices1974@gmail.com>
2025-01-17 10:09:08 +0200

    Fixed URL paths

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import { BookList, BookForm } from './components/books';
-import { AuthorList, AuthorForm } from './components/authors';
+
+import AuthorList from './components/authors/AuthorList';
+import AuthorForm from './components/authors/AuthorForm';
+import BookList from './components/books/BookList';
+import BookForm from './components/books/BookForm';
 
 function App() {
   return (
     <Router>
-<div>
-<nav>
-<ul>
-<li><Link to="/books">Knygos</Link></li>
-<li><Link to="/authors">Autoriai</Link></li>
-</ul>
-</nav>
-<Routes>
-<Route path="/books" element={<BookList />} />
-<Route path="/books/add" element={<BookForm />} />
-<Route path="/books/edit/:id" element={<BookForm isEdit={true} />} />
-<Route path="/authors" element={<AuthorList />} />
-<Route path="/authors/add" element={<AuthorForm />} />
-<Route path="/authors/edit/:id" element={<AuthorForm isEdit={true} />} />
-</Routes>
-</div>
-</Router>
+      <div>
+        <nav>
+          <ul>
+            <li><Link to="/books">Knygos</Link></li>
+            <li><Link to="/authors">Autoriai</Link></li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/books" element={<BookList />} />
+          <Route path="/books/add" element={<BookForm />} />
+          <Route path="/books/edit/:id" element={<BookForm isEdit={true} />} />
+          <Route path="/authors" element={<AuthorList />} />
+          <Route path="/authors/add" element={<AuthorForm />} />
+          <Route path="/authors/edit/:id" element={<AuthorForm isEdit={true} />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-export { default as BookList } from './BookList';
-export { default as BookForm } from './BookForm';
-export { default as AuthorList } from './AuthorList';
-export { default as AuthorForm } from './AuthorForm';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
