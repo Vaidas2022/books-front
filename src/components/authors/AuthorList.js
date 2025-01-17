15417ad4db609d0c56e7949a7f8a29f14c9e5b41
Vaidas Cesonis <vaices1974@gmail.com>
2025-01-17 12:14:25 +0200

    Refactored BookList ir AuthorList to show tables. Changed styles

--- src/components/authors/AuthorList.js
+++ src/components/authors/AuthorList.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
-    import axios from '../../AxiosConfig';
-     import { Link } from 'react-router-dom';
+import axios from '../../AxiosConfig';
+import { Link } from 'react-router-dom';
+import './AuthorList.css';
 
     function AuthorList() {
         const [authors, setAuthors] = useState([]);
@@ -29,19 +30,43 @@ import React, { useState, useEffect } from 'react';
 
 
         return (
-            <div>
-                <h2>Autorių sąrašas</h2>
-                <ul>
-                    {authors.map((author) => (
-                        <li key={author.id}>
-                            {author.name}
-                            <Link to={`/authors/edit/${author.id}`}>Redaguoti</Link>
-                            <button onClick={() => handleDelete(author.id)}>Trinti</button>
-                        </li>
+          <div>
+      <h2>Autorių sąrašas</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Vardas</th>
+            <th>Knygos</th>
+            <th>Veiksmai</th>
+          </tr>
+        </thead>
+        <tbody>
+          {authors.map((author) => (
+            <tr key={author.id}>
+              <td>{author.id}</td>
+              <td>{author.name}</td>
+              <td>
+                {author.books && author.books.length > 0 ? (
+                  <ul>
+                    {author.books.map((book) => (
+                      <li key={book.id}>{book.name}</li>
                     ))}
-                </ul>
-               <Link to="/authors/add">Pridėti autorių</Link>
-            </div>
+                  </ul>
+                ) : (
+                  "Neturi knygų"
+                )}
+              </td>
+              <td>
+                <Link to={`/authors/edit/${author.id}`}>Redaguoti</Link>
+                <button onClick={() => handleDelete(author.id)}>Trinti</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <Link to="/authors/add" className="add-button">Pridėti autorių</Link>
+    </div>
         );
     }
 
--- src/components/books/BookList.js
+++ src/components/books/BookList.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from '../../AxiosConfig';
 import { Link } from 'react-router-dom';
+import './BookList.css'
 
     function BookList() {
         const [books, setBooks] = useState([]);
@@ -28,19 +29,33 @@ import { Link } from 'react-router-dom';
         };
 
         return (
-                <div>
-                    <h2>Knygų sąrašas</h2>
-                    <ul>
-                        {books.map((book) => (
-                            <li key={book.book_id}>
-                                {book.book_name} (Autorius: {book.author_name || 'Nėra autoriaus'}) {/* Naudojame book_name ir author_name */}
-                                <Link to={`/books/edit/${book.book_id}`}> Redaguoti</Link> {/* Naudojame book_id */}
-                                <button onClick={() => handleDelete(book.book_id)}>Trinti</button>  {/* Naudojame book_id */}
-                            </li>
-                        ))}
-                    </ul>
-                    <Link to="/books/add">Pridėti knygą</Link>
-                </div>
+            <div>
+            <h2>Knygų sąrašas</h2>
+            <table>
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Pavadinimas</th>
+                  <th>Autorius</th>
+                  <th>Veiksmai</th>
+                </tr>
+              </thead>
+              <tbody>
+                {books.map((book) => (
+                  <tr key={book.book_id}>
+                    <td>{book.book_id}</td>
+                    <td>{book.book_name}</td>
+                    <td>{book.author_name || "Nėra autoriaus"}</td>
+                    <td>
+                      <Link to={`/books/edit/${book.book_id}`}>Redaguoti</Link>
+                      <button onClick={() => handleDelete(book.book_id)}>Trinti</button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+            <Link to="/books/add" className="add-button">Pridėti knygą</Link>
+          </div>
             );
     }
 
